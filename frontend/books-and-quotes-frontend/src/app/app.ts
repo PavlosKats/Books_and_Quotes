8ce860a700dfa10dadf1c04ac714b9d675e1a49f
Pavlos Katselis <paulkatselis@gmail.com>
2026-09-14 14:39:44 +0200

    Added Light/Dark themes

--- frontend/books-and-quotes-frontend/src/app/app.ts
+++ frontend/books-and-quotes-frontend/src/app/app.ts
@@ -18,6 +18,13 @@ export class App {
   authService = inject(AuthService);
   private router = inject(Router);
 
+  constructor() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      document.body.classList.add('dark-theme');
+    }
+  }
+
   protected logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
@@ -25,15 +32,26 @@ export class App {
 
 
   closeNavbar(): void {
-  const navbar = document.getElementById('mainNav');
-  const toggler = document.querySelector<HTMLButtonElement>('.navbar-toggler');
+    const navbar = document.getElementById('mainNav');
+    const toggler = document.querySelector<HTMLButtonElement>('.navbar-toggler');
 
-  if (!navbar || !toggler) {
-    return;
+    if (!navbar || !toggler) {
+      return;
+    }
+
+    if (navbar.classList.contains('show')) {
+      toggler.click();
+    }
   }
 
-  if (navbar.classList.contains('show')) {
-    toggler.click();
+  toggleTheme(): void {
+    document.body.classList.toggle('dark-theme');
+
+    const isDark = document.body.classList.contains('dark-theme');
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
   }
-}
+
+  isDarkTheme(): boolean {
+    return document.body.classList.contains('dark-theme');
+  }
 }
