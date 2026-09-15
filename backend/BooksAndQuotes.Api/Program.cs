c1469d11044a3418974c14c1d429f1021849a975
PavlosKats <119043016+PavlosKats@users.noreply.github.com>
2026-09-15 15:50:32 +0200

    Run migrations on startup

--- backend/BooksAndQuotes.Api/Program.cs
+++ backend/BooksAndQuotes.Api/Program.cs
@@ -109,4 +109,10 @@ app.UseRateLimiter();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
