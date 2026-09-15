b96e47d8b022774665ee9e3848c5f04f467adaaa
PavlosKats <119043016+PavlosKats@users.noreply.github.com>
2026-09-15 12:31:33 +0200

    Fix frontend build configuration

--- frontend/books-and-quotes-frontend/src/app/core/services/quote.service.ts
+++ frontend/books-and-quotes-frontend/src/app/core/services/quote.service.ts
@@ -2,7 +2,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 export interface Quote {
   id: number;
